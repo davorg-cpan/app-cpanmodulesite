ed359194243c9522c842625a3e7ab3c049d620ea
Dave Cross <dave@dave.org.uk>
2021-02-23 17:16:08 +0000

    Handle multiple files

--- lib/App/CPANModuleSite.pm
+++ lib/App/CPANModuleSite.pm
@@ -2,6 +2,7 @@ package App::CPANModuleSite;
 
 use MetaCPAN::Client;
 use Template;
+use Path::Iterator::Rule;
 use Moose;
 
 has distribution => (
@@ -46,15 +47,58 @@ sub _build_tt_config {
   my @modules = map { $self->metacpan->module($_) } @{ $release->provides };
 
   return {
-    INCLUDE_PATH => [ './site_src', './tt_lib' ],
-    OUTPUT_PATH => './docs',
-    WRAPPER => 'page.tt',
+    INCLUDE_PATH => $self->include_path,,
+    OUTPUT_PATH => $self->output_path,,
+    ( $self->wrapper ? ( WRAPPER => 'page.tt' ) : () ),
+    RELATIVE => 1,
     VARIABLES => {
       distribution => $distribution,
       release => $release,
       modules => \@modules,
     },
   }
+}
+
+has site_src => (
+  is => 'ro',
+  isa => 'Str',
+  lazy_build => 1,
+);
+
+sub _build_site_src {
+  return './site_src';
+}
+
+has include_path => (
+  is => 'ro',
+  isa => 'ArrayRef',
+  lazy_build => 1,
+);
+
+sub _build_include_path {
+  my $self = shift;
+
+  return [ $self->site_src, './tt_lib' ];
+}
+
+has output_path => (
+  is => 'ro',
+  isa => 'Str',
+  lazy_build => 1,
+);
+
+sub _build_output_path {
+  return './docs';
+}
+
+has wrapper => (
+  is => 'ro',
+  isa => 'Str',
+  lazy_build => 1,
+);
+
+sub _build_wrapper {
+  return 'page.tt';
 }
 
 around BUILDARGS => sub {
@@ -71,7 +115,24 @@ around BUILDARGS => sub {
 sub run {
   my $self = shift;
 
-  $self->tt->process('index.tt', undef, 'index.html')
+  my $finder = Path::Iterator::Rule->new->file;
+
+  for ( $finder->all($self->site_src, { relative => 1 }) ) {
+    if (/\.tt$/) {
+      $self->process_template($_);
+    } else {
+      $self->copy_file($_);
+    }
+  }
+}
+
+sub process_template {
+  my $self = shift;
+  my ($template) = @_;
+
+  my $output = $template =~ s/\.tt$//r;
+
+  $self->tt->process($template, undef, $output)
     or die $self->tt->error;
 }
 
